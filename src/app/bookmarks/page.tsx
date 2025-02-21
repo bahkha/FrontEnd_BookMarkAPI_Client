d9c66bdc835ae4986b517c62e771d2f21d15e55e
bahkha <haramx@naver.com>
2025-02-21 10:08:02 +0900

    error com

--- src/app/bookmarks/page.tsx
+++ src/app/bookmarks/page.tsx
@@ -4,11 +4,11 @@ import Bookmarks from "@/app/components/Bookmarks";
 import SearchForm from "@/app/components/SearchForm";
 
 type Props = {
-  searchParams: { page: string , query: string}
+  searchParams: Promise<{ page: string, query?: string }>
 }
 
-export default async function BookmarkPage(props: Props)  {
-  const { page = 1,  query = "" } = props.searchParams
+export default async function BookmarkPage(props:Props)  {
+  const { page = 1, query = "" } = await props.searchParams
   const bookmarksData: Promise<BookmarksResponse> = 
     getAllBookmarks(parseInt(String(page)), query)
   const bookmarksRes = await bookmarksData
@@ -17,8 +17,8 @@ export default async function BookmarkPage(props: Props)  {
 
   return (
     <div>
-      <SearchForm />
-      <Bookmarks bookmarks={bookmarksRes}  query={query}/>
+        <SearchForm />
+        <Bookmarks bookmarks={bookmarksRes} query={query} />
     </div>
   );
 }
